496ee4bfbd78d1ba74d882f9aa8d3be11eb3fa46
HuyVDSE <nhoxroney9x1@gmail.com>
2020-11-18 09:57:00 +0700

    update Main class

--- src/huyvd/util/MainMathUtil.java
+++ src/huyvd/util/MainMathUtil.java
@@ -32,7 +32,7 @@ public class MainMathUtil {
 	
 	System.out.println("expected: IllegalArgumentException: " + MathUtility.getFactorial(-5));
 	
-	System.out.println("Chuc cuoi tuan thu 9 vui ve");
+	System.out.println("Welcome to BlankSpace Repo!");
 	
     }
 }
