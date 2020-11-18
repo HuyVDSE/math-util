af43d2d3bfab1b9e64f5ef90b31aec99b9614c16
HuyVDSE <nhoxroney9x1@gmail.com>
2020-11-18 10:26:35 +0700

    add recursion function and still see GREEN again

--- src/huyvd/main/MathUtility.java
+++ src/huyvd/main/MathUtility.java
@@ -14,9 +14,9 @@ public class MathUtility {
     //Math.PI, .abs() .sqrt() .pow(a, b) a^b
     //pham la thu vien thi se xai static khi ko can luu info cho rieng minh
     //còn thư viện mà vẫn cần lưu info riêng thì vẫn none static như thường
-    
+
     public static final double PI = 3.1415;
-    
+
     //tien ich tinh gia thua n! = 1.2.3..n
     //m phaila tu 0 tro len 
     //0! = 1! = 1
@@ -24,22 +24,37 @@ public class MathUtility {
     //giai thua tang cuc nhanh, 21! long ko chua dc
     //bai nay minh se chan gia thua tu 0..20
     //am hoac 21 chui, ko them tinh
+//    public static long getFactorial(int n) {
+//	if (n < 0 || n > 20) {
+//	    throw new IllegalArgumentException("n must be between 0..20");
+//	    
+//	}//mai thay giang tiep, cam vang
+//	if (n == 0 || n == 1) {
+//	    return 1; //dac biet, return luon
+//	}
+//	//dua nao viet else o day, tru diem
+//	//mặc nhiên CPU đến dc đây là hiểu rằng n = 2..20
+//	long result = 1;
+//	for (int i = 2; i <= n; i++) {
+//	    result *= i;
+//	}
+//	return result;
+//    }
+    
+    //viết hàm bằng đệ quy cho PRO
     public static long getFactorial(int n) {
 	if (n < 0 || n > 20) {
-	    throw new IllegalArgumentException("n must be between 0..20");
-	    
-	}//mai thay giang tiep, cam vang
+	    throw new IllegalArgumentException("n must be between 0...20!");
+	}
 	if (n == 0 || n == 1) {
-	    return 1; //dac biet, return luon
+	    return 1;//dừng
 	}
-	//dua nao viet else o day, tru diem
-	//mặc nhiên CPU đến dc đây là hiểu rằng n = 2..20
-	long result = 1;
-	for (int i = 2; i <= n; i++) {
-	    result *= i;
-	}
-	return result;
+	return n * getFactorial(n - 1);
+	//gọi lại chính hàm của mình với quy mô khác, n - 1
+	//đi mãi sẽ về 1
     }
-    
-    
+    //5! = 5*4!; 4! = 4*3!; 3! = 3*2!; 2! = 2*1!
+    //1! = 1, đệ quy là gọi lại mình với quy mô nhỏ hơn
+    //kết luận: n! = n * (n-1)!
+    //phải có dừng, ko thì lặp vô tận
 }
